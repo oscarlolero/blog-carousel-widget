4ef85b001e6535ddf7d0c8284b93b6b1071651a8
Oscar Montes Camberos <o.montescamberos@gmail.com>
2024-05-24 11:31:09 -0600

    Deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/App.tsx'),
-      name: 'MyComponent',
-      fileName: (format) => `my-component.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
   plugins: [react()],
 })
